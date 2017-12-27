9358f058e763a585cf990e95cfc631d10eda7455
Yuri Khan <yurivkhan@gmail.com>
2017-12-27 23:16:43 +0700

    Track active tabs; only interfere if an active tab is closed

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 var tabsByWindow = {};
+var activeTabByWindow = {};
 
 function debug(...args) {
   // console.log(...args);
@@ -10,21 +11,26 @@ browser.tabs.query({})
     if (!tabsByWindow[tab.windowId]) {
       tabsByWindow[tab.windowId] = [];
     }
-    tabsByWindow[tab.windowId][tab.index] = tab.id;
+    tabsByWindow[tab.windowId][tab.index] = {id: tab.id, title: tab.title};
+    if (tab.active) {
+      activeTabByWindow[tab.windowId] = {id: tab.id, title: tab.title};
+    }
   })
 })
-.then(() => debug(tabsByWindow))
+.then(() => debug(tabsByWindow, activeTabByWindow))
 .catch(e => console.log(e));
 
 browser.tabs.onAttached.addListener((tabId, attachInfo) => {
   debug("onAttached", tabId, attachInfo);
-  tabsByWindow[attachInfo.newWindowId].splice(attachInfo.newPosition, 0, tabId);
+  browser.tabs.get(tabId).then(tab =>
+    tabsByWindow[attachInfo.newWindowId]
+    .splice(attachInfo.newPosition, 0, {id: tabId, title: tab.title}));
   debug(tabsByWindow);
 });
 
 browser.tabs.onCreated.addListener(tab => {
   debug("onCreated", tab);
-  tabsByWindow[tab.windowId].splice(tab.index, 0, tab.id);
+  tabsByWindow[tab.windowId].splice(tab.index, 0, {id: tab.id, title: tab.title});
   debug(tabsByWindow);
 });
 
@@ -36,25 +42,33 @@ browser.tabs.onDetached.addListener((tabId, detachInfo) => {
 
 browser.tabs.onMoved.addListener((tabId, moveInfo) => {
   debug("onMoved", tabId, moveInfo);
+  let tab = tabsByWindow[moveInfo.windowId][moveInfo.fromIndex];
   tabsByWindow[moveInfo.windowId].splice(moveInfo.fromIndex, 1);
-  tabsByWindow[moveInfo.windowId].splice(moveInfo.toIndex, 0, tabId);
+  tabsByWindow[moveInfo.windowId].splice(moveInfo.toIndex, 0, tab);
   debug(tabsByWindow);
 });
 
-function findTab(tabId) {
-  return Object.entries(tabsByWindow)
-    .map(([windowId, tabs]) => ({windowId, index: tabs.indexOf(tabId)}))
-    .find(({windowId, index}) => index != -1);
+browser.tabs.onActivated.addListener(({tabId, windowId}) => {
+  debug("onActivated", tabId, windowId);
+  browser.tabs.get(tabId).then(tab =>
+    activeTabByWindow[windowId] = {id: tabId, title: tab.title});
+  debug(activeTabByWindow);
+});
+
+function findTab(windowId, tabId) {
+  return (tabsByWindow[windowId] || []).findIndex(e => e.id == tabId);
 }
 
 browser.tabs.onRemoved.addListener((tabId, removeInfo) => {
   debug("onRemoved", tabId, removeInfo);
-  if (!removeInfo.isWindowClosing) {
-    var {windowId, index} = findTab(tabId);
+  let windowId = removeInfo.windowId;
+  let index = findTab(windowId, tabId);
+  if (!removeInfo.isWindowClosing
+      && tabId == activeTabByWindow[windowId].id) {
     debug(windowId, index);
     tabsByWindow[windowId].splice(index, 1);
     browser.tabs.update(
-      tabsByWindow[windowId][index == 0 ? 0 : index - 1],
+      tabsByWindow[windowId][index <= 0 ? 0 : index - 1].id,
       {active: true})
     .catch(e => console.log(e));
   } else {
