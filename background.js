62d2742f87f55258613d3b8c1dfc7495efa8915f
Yuri Khan <yuri.v.khan@gmail.com>
2019-02-03 22:00:47 +0700

    Reimplement using tab successor API

--- background.js
+++ background.js
@@ -1,78 +1,40 @@
-var tabsByWindow = {};
-var activeTabByWindow = {};
-
-function debug(...args) {
-  // console.log(...args);
-}
-
+// On startup, set each tab’s successor to its preceding tab.
 browser.tabs.query({})
 .then(tabs => {
+  var tabsByWindow = {}; // type: {[WindowID]: {[TabIndex]: Tab}}
   tabs.forEach(tab => {
     if (!tabsByWindow[tab.windowId]) {
       tabsByWindow[tab.windowId] = [];
     }
-    tabsByWindow[tab.windowId][tab.index] = {id: tab.id, title: tab.title};
-    if (tab.active) {
-      activeTabByWindow[tab.windowId] = {id: tab.id, title: tab.title};
-    }
+    tabsByWindow[tab.windowId][tab.index] = tab.id;
+  });
+  Object.entries(tabsByWindow).forEach(([windowId, windowTabIds]) => {
+    var reversedTabIds = windowTabIds.slice();
+    reversedTabIds.reverse();
+    browser.tabs.moveInSuccession(reversedTabIds);
   })
 })
-.then(() => debug(tabsByWindow, activeTabByWindow))
-.catch(e => console.log(e));
+.catch(console.log);
+
+
+function adjustSuccessor(windowId, tabIndex, tabId) {
+  browser.tabs.query({windowId: windowId, index: (tabIndex > 0 ? tabIndex - 1 : 1)})
+  .then(([referenceTab]) => {
+    browser.tabs.moveInSuccession([tabId], referenceTab.id,
+                                  {insert: tabIndex > 0, append: tabIndex <= 0});
+  })
+  .catch(console.log);
+}
+
 
 browser.tabs.onAttached.addListener((tabId, attachInfo) => {
-  debug("onAttached", tabId, attachInfo);
-  browser.tabs.get(tabId).then(tab =>
-    tabsByWindow[attachInfo.newWindowId]
-    .splice(attachInfo.newPosition, 0, {id: tabId, title: tab.title}));
-  debug(tabsByWindow);
-});
+  adjustSuccessor(attachInfo.newWindowId, attachInfo.newPosition, tabId);
+})
 
 browser.tabs.onCreated.addListener(tab => {
-  debug("onCreated", tab);
-  tabsByWindow[tab.windowId].splice(tab.index, 0, {id: tab.id, title: tab.title});
-  debug(tabsByWindow);
-});
-
-browser.tabs.onDetached.addListener((tabId, detachInfo) => {
-  debug("onDetached", tabId, detachInfo);
-  tabsByWindow[detachInfo.oldWindowId].splice(detachInfo.oldPosition, 1);
-  debug(tabsByWindow);
+  adjustSuccessor(tab.windowId, tab.index, tab.id);
 });
 
 browser.tabs.onMoved.addListener((tabId, moveInfo) => {
-  debug("onMoved", tabId, moveInfo);
-  let tab = tabsByWindow[moveInfo.windowId][moveInfo.fromIndex];
-  tabsByWindow[moveInfo.windowId].splice(moveInfo.fromIndex, 1);
-  tabsByWindow[moveInfo.windowId].splice(moveInfo.toIndex, 0, tab);
-  debug(tabsByWindow);
-});
-
-browser.tabs.onActivated.addListener(({tabId, windowId}) => {
-  debug("onActivated", tabId, windowId);
-  browser.tabs.get(tabId).then(tab =>
-    activeTabByWindow[windowId] = {id: tabId, title: tab.title});
-  debug(activeTabByWindow);
-});
-
-function findTab(windowId, tabId) {
-  return (tabsByWindow[windowId] || []).findIndex(e => e.id == tabId);
-}
-
-browser.tabs.onRemoved.addListener((tabId, removeInfo) => {
-  debug("onRemoved", tabId, removeInfo);
-  let windowId = removeInfo.windowId;
-  let index = findTab(windowId, tabId);
-  if (!removeInfo.isWindowClosing
-      && tabId == activeTabByWindow[windowId].id) {
-    debug(windowId, index);
-    tabsByWindow[windowId].splice(index, 1);
-    browser.tabs.update(
-      tabsByWindow[windowId][index <= 0 ? 0 : index - 1].id,
-      {active: true})
-    .catch(e => console.log(e));
-  } else {
-    tabsByWindow[windowId].splice(index, 1);
-  }
-  debug(tabsByWindow);
+  adjustSuccessor(moveInfo.windowId, moveInfo.toIndex, tabId);
 });
